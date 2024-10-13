a7b91fbf52af2a502fa2cb7608139fec6faddce0
JuanFco21 <pacotab11@gmail.com>
2024-10-12 22:07:00 -0600

    Implementación de localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,10 @@ export class GifsService {
     private apiKey: string = 'I8xZJ3keSqKFeIedYwPG2DwN9m6CXP44';
     private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+        this.loadLocalStorage();
+        //console.log('Gifs Service Ready');
+    }
 
     get tagsHistory() {
         return [...this._tagsHistory];
@@ -26,6 +29,19 @@ export class GifsService {
 
         this._tagsHistory.unshift(tag);
         this._tagsHistory = this.tagsHistory.splice(0, 10);
+        this.saveLocalStorage();
+    }
+
+    private saveLocalStorage(): void {
+        localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+    }
+
+    private loadLocalStorage(): void {
+        if (!localStorage.getItem('history')) return;
+
+        this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+        if (this._tagsHistory.length === 0) return;
+        this.searchTag(this._tagsHistory[0]);
     }
 
     searchTag(tag: string): void {
