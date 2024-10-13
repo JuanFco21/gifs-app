f1225efe4ffe7f1f53ca840f6832d57b9fa0957a
JuanFco21 <pacotab11@gmail.com>
2024-10-12 18:21:39 -0600

    Creación de home, card list y search box

---
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'card-list',
+    templateUrl: 'card-list.component.html'
+})
+
+export class CardList {
+    constructor() { }
+
+    ngOnInit() { }
+}
